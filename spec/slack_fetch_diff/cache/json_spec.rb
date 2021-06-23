b933353d74147b94e2c9627efe7fc97dc7f39b06
dnond <tarosuk@gmail.com>
2021-06-24 07:45:48 +0900

    Fix to remove cache file after spec

--- spec/slack_fetch_diff/cache/json_spec.rb
+++ spec/slack_fetch_diff/cache/json_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe SlackFetchDiff::Cache::Json do
   let(:key) { Faker::Lorem.characters(number: 10) }
   let(:set_value) { Faker::Lorem.character }
 
+  after do
+    model = SlackFetchDiff::Cache::Json.new(cache_dir: cache_dir)
+    cache_file = model.send(:cache_file_of, key)
+    File.delete(cache_file)
+  end
+
   it 'set value is equal to get value' do
     cache = SlackFetchDiff::Cache::Json.new(cache_dir: cache_dir)
     cache.set(key, set_value)
